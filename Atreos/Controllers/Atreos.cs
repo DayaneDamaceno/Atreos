107f745c9f7028912aee353f69f590cecf3b007d
Arthur-Fugolim <arthurfugolim.lima@hotmail.com>
2022-11-11 14:35:01 -0300

    [FEAT] - Tela Aluno

---
+++ Atreos/Controllers/Atreos.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atreos.Controllers
+{
+    public class Atreos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Alunos()
+        {
+            return View();
+        }
+    }
+}
